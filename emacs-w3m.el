b0e257d66bac9adeaf08347eee2f700a2537d441
Drazen Pavetic <drazen.pavetic@zrinski.org>
2014-09-24 10:11:31 +0200

    Added (require 'w3m-search) to w3m init el file.

--- emacs-w3m.el
+++ emacs-w3m.el
@@ -1,6 +1,7 @@
 ;; w3m init file
 
 ; (setq w3m-home-page "http://emacs-w3m.namazu.org/")
+(require 'w3m-search)
 (add-to-list 'w3m-search-engine-alist
 	     '("duckduckgo" "https://duckduckgo.com/?q=%s"))
 (setq w3m-search-default-engine "duckduckgo")
